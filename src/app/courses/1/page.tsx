e719c04744773b33bece3a307fe8bbad33e26b6a
HaiderMalik12 <haidermalik504@gmail.com>
2023-01-20 20:10:48 +0500

    finish file based routing

---
+++ src/app/courses/1/page.tsx
@@ -0,0 +1,8 @@
+export default function React(){
+    return (
+        <div>
+          <h1>React Course</h1>
+          <p>ID : 1</p>
+        </div>
+    )
+}
---
+++ src/app/courses/nodejs/page.tsx
@@ -0,0 +1,7 @@
+export default function NodejsCourse(){
+    return (
+        <div>
+          <h1>Nodejs Course</h1>
+        </div>
+    )
+}
