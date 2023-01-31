9d72bf44ac90eace5838e34e95b1f96f31d8f063
HaiderMalik12 <haidermalik504@gmail.com>
2023-01-31 16:59:53 +0500

    completed data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,21 @@ import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+const getData = async() => {
+  const results = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+  return results.json();
+}
+
+export default async function Home() {
+  const post = await getData();
   return (
     <main>
       <Link href="/">Home</Link> | 
       <Link href="/dashboard">Dashboard</Link> |
       <Link href ="/courses">Courses</Link>
+
+      <p>{post.title}</p>
+      <p>{post.body}</p>
     </main>
   )
 }
